b3842c33c51b1a4b7b29c28c755680f5155bdee8
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2023-08-16 12:45:35 +0100

    Modified encrypt password file

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-"""python bycrypt"""
+"""
+Defines a hash_password function to return a hashed password
+"""
 import bcrypt
+from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password"""
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    """
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks password"""
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
